e3c29a1a128c51186b182cc282ebe2c7d27a9622
Natalia-Marquez <natalia.marquez@utec.edu.pe>
2019-11-01 17:29:01 -0500

    add Class producto - slot

---
+++ robot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by carol on 01/11/2019.
+//
+
+#include "robot.h"
---
+++ robot.h
@@ -0,0 +1,14 @@
+//
+// Created by carol on 01/11/2019.
+//
+
+#ifndef PROYECTO1_ROBOT_H
+#define PROYECTO1_ROBOT_H
+
+
+class robot {
+
+};
+
+
+#endif //PROYECTO1_ROBOT_H
