6de7c9e33987db89af7d0213c938fe2187dd79c8
AlfieriPeñaloza <christian.penaloza@utec.edu.pe>
2019-11-01 19:01:56 -0500

    Agregué clase slot

---
+++ slot.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Alfieri Peñaloza on 1/11/2019.
+//
+
+#include "slot.h"
+#include "tipos.h"
---
+++ slot.h
@@ -0,0 +1,14 @@
+//
+// Created by Alfieri Peñaloza on 1/11/2019.
+//
+
+#ifndef PROYECTO1_SLOT_H
+#define PROYECTO1_SLOT_H
+#include "tipos.h"
+
+class slot {
+
+};
+
+
+#endif //PROYECTO1_SLOT_H
